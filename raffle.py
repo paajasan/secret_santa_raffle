24c13b4620bf77ad3e690ef7ec4665ee3e8f9b33
Santeri Paajanen <paajanensanteri@gmail.com>
2022-07-20 20:46:53 +0300

    Updated to better raffle

--- raffle.py
+++ raffle.py
@@ -8,76 +8,70 @@ import getpass
 from email.message import EmailMessage
 
 
-def laheta(nimi, sapo, server):
+def send_email(name, email, server):
     msg = EmailMessage()
-    msg.set_content("You are the secret santa for %s\n\n Kindest regards\nSanterin koodi\n\n\n\n"%nimi +\
+    msg.set_content("You are the secret santa for %s\n\n Kindest regards\nSanterin koodi\n\n\n\n"%name +\
                     "---------------------" +\
                     "This is an automated message, please do not reply or mörökölli will viedä you")
     msg['Subject'] = 'Secret santa raffle'
     msg['From'] = "erkki.esimerkki@domain.com"
-    msg['To'] = sapo
+    msg['To'] = email
 
     server.send_message(msg)
 
 
 def get_contacts(fname):
-    nimet = []
-    sapot = []
+    names= []
+    emails = []
     with open(fname) as f:
         for line in f:
             if(not line.strip() or line.strip().startswith("#")):
                 continue
             parts = line.split()
-            nimet.append(parts[0])
-            sapot.append(parts[1])
+            names.append(parts[0])
+            emails.append(parts[1])
 
-    return nimet, sapot
-
-
-def arvo(nimet, sapot):
-    ostettava = []
-
-    valinnat = nimet.copy()
+    return names, emails
 
 
-    for i in range(len(nimet)):
-        c = random.choice(valinnat)
-        while c == nimet[i]:
-            if(len(valinnat)==1):
-                print("Arvotaan ostajalle %s, mutta vain hän jäljellä")
-                print("Arvotaanpas uudelleen")
-                return arvo(nimet, sapot)
-            c = random.choice(valinnat)
-        ostettava.append(c)
-        valinnat.remove(c)
+def raffle(names, emails):
+    # shuffle data together
+    data = list(zip(names, emails))
+    random.shuffle(data)
+    names_shuffl, email_shuffl = list(zip(*data))
 
-    return ostettava, sapot
+    # Shift emails by one
+    new_emails = list(email_shuffl[1:])
+    new_emails.append(email_shuffl[0])
+
+    return names_shuffl, new_emails
 
 
-def send(ostettava, sapot):
+def send(names, emails):
     # This example works for helsinki.fi emails. Change to match your server.
     s = smtplib.SMTP("smtp.helsinki.fi", port=587)
     s.starttls()
     print("Authenticating helsinki.fi office365 service")
     s.login(input("AD-tunnus: ")+"@ad.helsinki.fi", getpass.getpass("Salakala: "))
 
-    for ostett, sapo in zip(ostettava, sapot):
-        laheta(ostett, sapo, s)
+    for name, email in zip(names, emails):
+        send_email(name, email, s)
 
     s.quit()
 
 
-def print_debug(ostettava, sapot):
-    for ostett, sapo in zip(ostettava, sapot):
-        print(ostett, sapo)
+def print_debug(names, emails):
+    for name, email in zip(names, emails):
+        print(name, email)
 
 
 if __name__ == '__main__':
     # Get random seed from e.g. random.org and put here. Allows for reproducing in case of problems.
-    random.seed(340338373)
-    nimet, sapot = get_contacts("nimet_ja_sapot.txt")
-    ostettava, ostaja_sapo = arvo(nimet,sapot)
+    # Comment to have unreproducible results
+    random.seed(1337)
+    names, emails = get_contacts("names_and_emails.txt")
+    buyee_names, buyer_emails = raffle(names,emails)
     # Send the results
-    #send(ostettava, sapot)
+    #send(buyee_names, buyer_emails)
     # Only print
-    print_debug(ostettava, ostaja_sapo)
+    print_debug(buyee_names, buyer_emails)
